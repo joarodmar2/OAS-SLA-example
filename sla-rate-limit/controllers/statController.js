5e0661b0382b2ca092b8d595451c7fd0d0042948
Joaquín <joarodmar2@us.es>
2023-05-12 12:35:36 +0200

    push

---
+++ sla-rate-limit/controllers/statController.js
@@ -0,0 +1,14 @@
+export function getStats(_, res) {
+    res.send([{ "period": 2012, "territory": "Sevilla" }])
+}
+
+
+
+export function findByid(_, res) {
+    res.send({ "period": 2012, "territory": "Sevilla" })
+}
+
+
+
+
+
